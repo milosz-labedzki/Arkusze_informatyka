faf77301f1091d136b03b520877d8a7f41796bdb
Miłosz Łabędzki <miloszlabedzki7@gmail.com>
2026-09-22 18:39:11 +0200

    add some files

--- Arkusz1/anagramy_c.py
+++ Arkusz1/anagramy_c.py
@@ -1,14 +1,18 @@
 lista = []
 lista_anagramow = []
+zliczenia = {}
 with open("wyrazy.txt","r",encoding="UTF-8") as f:
     for line in f:
         line = line.strip()
         line = line.split()
         slowo = line[0]
-        lista.append(slowo)  
-        for i in range(len(lista)):
-            for j in range(i+1, len(lista)):
-                if(sorted(lista[i])==sorted(lista[j])):
-                    lista_anagramow.append((lista[i],lista[j]))
-    lista_anagramow = set(lista_anagramow)
-print(print(lista_anagramow))
+        lista.append(slowo)
+        klucz = "".join(sorted(slowo))
+
+        if(klucz not in zliczenia):
+            zliczenia[klucz] = []
+        zliczenia[klucz].append(slowo)
+        
+            
+print(zliczenia)
+
